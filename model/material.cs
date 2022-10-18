be27757c1cced161445b9085110329ab478af4f4
benja-min12 <61527051+benja-min12@users.noreply.github.com>
2022-10-18 20:47:23 -0300

    Untrack files in .gitignore

--- model/material.cs
+++
@@ -1,9 +0,0 @@
-public class Material
-{
-    public int Id { get; set; }
-    public string code { get; set; }
-    public string name { get; set; }
-    public int price { get; set; }
-    public int quantity { get; set; }
-
-}
--- model/project.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-public class Project
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public enum Status { Active, Inactive }
-    public IList<Task> tasks { get; set; }
-}
