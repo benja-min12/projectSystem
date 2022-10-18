0e4301727a9a76cf74dfebb08e0bba4a59ca94a0
benja-min12 <61527051+benja-min12@users.noreply.github.com>
2022-10-18 20:51:18 -0300

    CambiosBaseDatos

---
+++ Migrations/20221014215032_fixtables.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaProyecto.Migrations
+{
+    public partial class fixtables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_materials_tasks_TaskId",
+                table: "materials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_materials_TaskId",
+                table: "materials");
+
+            migrationBuilder.DropColumn(
+                name: "TaskId",
+                table: "materials");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TaskId",
+                table: "materials",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_materials_TaskId",
+                table: "materials",
+                column: "TaskId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_materials_tasks_TaskId",
+                table: "materials",
+                column: "TaskId",
+                principalTable: "tasks",
+                principalColumn: "Id");
+        }
+    }
+}
